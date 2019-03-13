46ebdbf2afd4dcd10507a2975f0778dc5c95d2a3
Karik Isichei <karik.isichei@gmail.com>
2019-03-13 10:22:34 +0000

    rework

--- iam_builder/command_line.py
+++ iam_builder/command_line.py
@@ -1,63 +1,8 @@
 import argparse
-import json
-import yaml
-
-from templates import (
-    iam_base_template as iam,
-    iam_lookup,
-    get_pass_role_to_glue_policy,
-    get_read_only_policy,
-    get_read_write_policy,
-    get_s3_list_bucket_policy
-)
-import copy
+from iam_builder.iam_builder import build_iam_policy
 
 def main(config_path, out_path):
-    # Assume config is a yaml file if not json
-    if config_path.endswith('.json'):
-        with open(config_path, 'r') as f:
-            config = json.load(f)
-    else:
-        with open(config_path, 'r') as f:
-            config = yaml.load(f)
-
-    # Define if has athena permission
-    if 'athena' in config:
-        iam['Statement'].extend(iam_lookup["athena_read_access"])
-
-        if config['athena']['read_write_access']:
-            iam['Statement'].extend(iam_lookup["athena_write_access"])
-
-    # Test to run glue jobs
-    if 'run_glue_job' in config:
-        iam['Statement'].extend(iam_lookup['run_glue_job'])
-        # Add ability to pass itself to glue job
-        pass_role = get_pass_role_to_glue_policy(config['run_glue_job']['iam_role'])
-        iam['Statement'].append(pass_role)
-
-    # Deal with read only access
-    list_buckets = []
-    if 'read_only_s3_access' in config:
-        s3_read_only = get_read_only_policy(config['read_only_s3_access'])
-        iam['Statement'].append(s3_read_only)
-
-        # Get buckets to list
-        list_buckets.extend(['arn:aws:s3:::' + p.split('/')[0] for p in config['read_only_s3_access']])
-
-    # Deal with write only access
-    if 'read_write_s3_access' in config:
-        s3_read_write = get_read_write_policy(config['read_write_s3_access'])
-        iam['Statement'].append(s3_read_write)
-
-        # Get buckets to list
-        list_buckets.extend(['arn:aws:s3:::' + p.split('/')[0] for p in config['read_write_s3_access']])
-
-    if list_buckets:
-        s3_list_bucket = get_s3_list_bucket_policy(list_buckets)
-        iam['Statement'].append(s3_list_bucket)
-    
-    with open(out_path, "w+") as outfile:
-        json.dump(iam, outfile, indent=4, separators=(',', ': '))
+    build_iam_policy(config_path, out_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
