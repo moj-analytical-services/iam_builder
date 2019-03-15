e49d149427fa56f62cca3507a631e64fb965b053
Karik Isichei <karik.isichei@gmail.com>
2019-03-15 12:27:40 +0000

    trying work around

--- iam_builder/command_line.py
+++ iam_builder/command_line.py
@@ -1,12 +1,12 @@
 import argparse
 from iam_builder.iam_builder import build_iam_policy
 
-def main(config_path, out_path):
-    build_iam_policy(config_path, out_path)
-
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", help="path to your yaml/json config")
     parser.add_argument("-o", "--output", help="output_path")
     args = parser.parse_args()
-    main(args.config, args.output)
+    build_iam_policy(args.config, args.output)
+
+if __name__ == "__main__":
+    main()
