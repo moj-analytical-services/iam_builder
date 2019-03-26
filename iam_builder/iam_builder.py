9a7866fb242db2e04f8cc8bf4dbb04f2a6f5975d
Karik Isichei <karik.isichei@gmail.com>
2019-03-26 14:15:56 +0000

    updating configuration yaml/json naming conventions also fixes #2

--- iam_builder/iam_builder.py
+++ iam_builder/iam_builder.py
@@ -24,43 +24,44 @@ def build_iam_policy(config_path, out_path):
     if 'athena' in config:
         iam['Statement'].extend(iam_lookup["athena_read_access"])
 
-        if config['athena']['read_write_access']:
+        if config['athena']['write']:
             iam['Statement'].extend(iam_lookup["athena_write_access"])
 
     # Test to run glue jobs
-    if 'run_glue_job' in config:
-        iam['Statement'].extend(iam_lookup['run_glue_job'])
+    if 'glue_job' in config:
+        iam['Statement'].extend(iam_lookup['glue_job'])
         # Add ability to pass itself to glue job
-        pass_role = get_pass_role_to_glue_policy(config['run_glue_job']['iam_role'])
+        pass_role = get_pass_role_to_glue_policy(config['glue_job']['iam_role_name'])
         iam['Statement'].append(pass_role)
 
     # Deal with read only access
     list_buckets = []
-    if 'read_only_s3_access' in config:
-        s3_read_only = get_read_only_policy(config['read_only_s3_access'])
-        iam['Statement'].append(s3_read_only)
+    if 's3' in config:
+        if 'read_only' in config['s3']:
+            s3_read_only = get_read_only_policy(config['read_only_s3_access'])
+            iam['Statement'].append(s3_read_only)
 
-        # Get buckets to list
-        list_buckets.extend([p.split('/')[0] for p in config['read_only_s3_access']])
+            # Get buckets to list
+            list_buckets.extend([p.split('/')[0] for p in config['read_only_s3_access']])
 
-    if 'write_only_s3_access' in config:
-        s3_write_only = get_write_only_policy(config['write_only_s3_access'])
-        iam['Statement'].append(s3_write_only)
+        if 'write_only' in config['s3']:
+            s3_write_only = get_write_only_policy(config['write_only_s3_access'])
+            iam['Statement'].append(s3_write_only)
 
-        # Get buckets to list
-        list_buckets.extend([p.split('/')[0] for p in config['write_only_s3_access']])
+            # Get buckets to list
+            list_buckets.extend([p.split('/')[0] for p in config['write_only_s3_access']])
 
-    # Deal with write only access
-    if 'read_write_s3_access' in config:
-        s3_read_write = get_read_write_policy(config['read_write_s3_access'])
-        iam['Statement'].append(s3_read_write)
+        # Deal with write only access
+        if 'read_write' in config['s3']:
+            s3_read_write = get_read_write_policy(config['read_write_s3_access'])
+            iam['Statement'].append(s3_read_write)
 
-        # Get buckets to list
-        list_buckets.extend([p.split('/')[0] for p in config['read_write_s3_access']])
+            # Get buckets to list
+            list_buckets.extend([p.split('/')[0] for p in config['read_write_s3_access']])
 
     if list_buckets:
         s3_list_bucket = get_s3_list_bucket_policy(list_buckets)
         iam['Statement'].append(s3_list_bucket)
-    
+
     with open(out_path, "w+") as outfile:
         json.dump(iam, outfile, indent=4, separators=(',', ': '))
