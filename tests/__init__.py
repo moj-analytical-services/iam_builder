d4b10970eb9f4f3d0771da037782da996e632e79
Karik Isichei <karik.isichei@gmail.com>
2019-03-27 11:30:31 +0000

    stashing

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,23 +1,28 @@
+import json
+import yaml
+
 import boto3
 from time import sleep
 
+from iam_builder.iam_builder import build_iam_policy
+
+
 iam = boto3.client('iam')
 
-role_name = 'iam_builder_test_role'
-
-def remove_all_roles_from_policy(role_name):
+def remove_all_polices_from_role(role_name):
     list_of_policies = iam.list_role_policies(RoleName=role_name)
     for p in list_of_policies:
         response = iam.delete_role_policy(
             RoleName=role_name,
-            PolicyName='read_only'
+            PolicyName=p
         )
 
-def wait_for_policy_role_change(role_name, policy_name = None, num_checks = 5, wait_time = 2):
+def wait_for_policy_role_change(role_name, policy_name=None, num_checks=5, wait_time=2):
     """
     Wait for a policy to either have no policies or have a policy added.
     If policy_name is None then excepts no policies to be added to role.
-    """ 
+    """
+
     i = 0
     if policy_name:
         test_fun = lambda p, l: p in l
@@ -37,3 +42,19 @@ def wait_for_policy_role_change(role_name, policy_name = None, num_checks = 5, w
     if i >= num_checks:
         list_policy_names = ', '.join(response['PolicyNames'])
         raise TimeoutError(f"Timed Out: Policy change did not take affect. PolicyNames: {list_policy_names}. Try upping the number of wait_time or num_check args.")
+
+def init_role_and_add_single_policy_from_config(role_name, config_path):
+    remove_all_polices_from_role(test_role_name)
+    wait_for_policy_role_change(role_name, policy_name=None)
+
+    with open(config_path, 'r') as f:
+        policy_config = yaml.load(f)
+
+    policy_doc = build_iam_policy(policy_config)
+    iam.put_role_policy(
+        RoleName=test_role_name,
+        PolicyName='test_policy',
+        PolicyDocument=json.dumps(policy_doc)
+    )
+
+    wait_for_policy_role_change(role_name, policy_name='test_policy')
--- tests/test_iam_configurations.py
+++ tests/test_iam_configurations.py
@@ -1,21 +1,31 @@
 """
 Testing IAM comfigurations work as expected
 """
-
 import unittest
+
 from etl_manager.etl import GlueJob
 from gluejobutils import s3
 
-from tests import iam, role_name, wait_for_policy_role_change,
+from tests import (
+    boto3,
+    iam,
+    init_role_and_add_single_policy_from_config
+)
 
 class TestS3Config(unittest.TestCase):
     """
-    Test 
+    Test S3 configuration
     """
     def test_read_only(self):
         """
         Test read only access
         """
+        c_path = 'tests/test_configs/readonly.yaml'
+        init_role_and_add_single_policy_from_config('iam_builder_test_role', c_path)
+
+
+        s3.read_json_from_s3('s3://alpha-test-iam-builer/read_only/test.json')
+        s3.read_json_from_s3('s3://alpha-test-iam-builer/write_only/test.json')
 
 
 
