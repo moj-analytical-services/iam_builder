e041c9c186647463f0193b4801f3875984bc0b07
jhpyke <jacob.hamblin-pyke@digital.justice.gov.uk>
2024-12-16 11:13:12 +0000

    Make exception thrown generic

--- iam_builder/iam_schema.py
+++ iam_builder/iam_schema.py
@@ -4,7 +4,8 @@ from iam_builder.exceptions import IAMValidationError
 
 try:
     import pkg_resources
-except ModuleNotFoundError:
+except Exception as e:
+    print("Error importing pkg_resources", e)
     import importlib
 
 
